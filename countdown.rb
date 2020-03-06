72182a9d05d5754ffa66e21003d8b1618245d852
Bikram Sodhi <biksodhi@gmail.com>
2020-03-06 21:38:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,15 @@
 #write your code here
 
-def countdown(digits)
+def countdown(digits,should_sleep = false)
   until digits == 0
     puts "#{digits} SECOND(S)!"
+    if should_sleep
+      sleep(1)
     digits -= 1
   end
   return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(digitas)
+  countdown(digits, true)
 end
